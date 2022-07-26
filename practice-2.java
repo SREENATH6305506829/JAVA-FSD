2e23db3157f00827eedaee9db7b569c5afd61079
SREENATH6305506829 <sreenathnanabala@gmail.com>
2022-07-26 15:46:06 +0530

    Practice-1,practice-2,practice-3,practice-4,practice-5,practice-6,practice-7,practice-8,practice-9

---
+++ practice-2.java
@@ -0,0 +1,22 @@
+package Practice;
+import java.util.Arrays;
+public class KthSmallestElementExample {
+
+	public static int KthSmallest(Integer[] arr,int k) {
+		
+		Arrays.sort(arr);
+		
+		
+		return arr[k-1]; 
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Integer arr[]= new Integer[] {28,16,46,38,55,89,83,30};
+		
+		int k=3; 
+		
+		System.out.println("Kth Smallest Element is : "+ KthSmallest(arr, k) );
+	}
+}
---
+++ practice-9.java
@@ -0,0 +1,29 @@
+package Practice;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExample {
+	public static void main(String[] args) {
+		Queue<String> locationQueue= new LinkedList<String>();
+		
+		locationQueue.add("India");
+		locationQueue.add("China");
+		locationQueue.add("Japan");
+		locationQueue.add("Russia");
+		locationQueue.add("Indonesia");
+		locationQueue.add("US");
+		
+		System.out.println("Queue is : "+locationQueue);
+		
+		
+		//find head of queue
+		System.out.println("Head of Queue: "+locationQueue.peek());
+		
+		locationQueue.remove();
+		
+		System.out.println("After Removing Head: "+locationQueue);
+		System.out.println("Size of Queue : " + locationQueue.size());
+		
+	}
+
+}
